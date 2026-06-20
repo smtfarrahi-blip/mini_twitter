89609d61f043fac4a54001b53c74ad32edb5931c
smtfarrahi-blip <s.mt.farrahi@gmail.com>
2026-06-20 23:35:23 +0330

    add glass ui

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, session, redirect
+from flask import Blueprint, request, session, redirect, render_template
 from database import create_user, login_user
 
 auth = Blueprint("auth", __name__)
@@ -7,65 +7,40 @@ auth = Blueprint("auth", __name__)
 @auth.route("/register", methods=["GET","POST"])
 def register():
 
-    if request.method=="POST":
+    if request.method == "POST":
 
-        username=request.form["username"]
-        password=request.form["password"]
+        username = request.form["username"]
+        password = request.form["password"]
 
         if create_user(username,password):
-            session["user"]=username
+            session["user"] = username
             return redirect("/")
 
-        return "username already exists"
+        return "این یوزرنیم قبلاً گرفته شده"
 
-    return """
-    <h2>ثبت نام Zgrom</h2>
+    return render_template("register.html")
 
-    <form method="post">
-
-    <input name="username" placeholder="username">
-
-    <input name="password" placeholder="password" type="password">
-
-    <button>
-    ثبت نام
-    </button>
-
-    </form>
-    """
 
 
 @auth.route("/login", methods=["GET","POST"])
 def login():
 
-    if request.method=="POST":
+    if request.method == "POST":
 
-        username=request.form["username"]
-        password=request.form["password"]
+        username = request.form["username"]
+        password = request.form["password"]
+
 
         if login_user(username,password):
-            session["user"]=username
+            session["user"] = username
             return redirect("/")
 
-        return "wrong username or password"
+
+        return "یوزرنیم یا رمز اشتباه است"
 
 
-    return """
-    <h2>ورود Zgrom</h2>
+    return render_template("login.html")
 
-    <form method="post">
-
-    <input name="username">
-
-    <input name="password" type="password">
-
-    <button>
-    ورود
-    </button>
-
-    </form>
-
-    """
 
 
 @auth.route("/logout")
