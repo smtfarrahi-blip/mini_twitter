45b3867c1708c6f3cbe1e30aa743df7f9a298e81
smtfarrahi-blip <s.mt.farrahi@gmail.com>
2026-06-21 10:51:17 +0330

    fix database syntax

--- database.py
+++ database.py
@@ -1,5 +1,5 @@
-import sqlite3
-from werkzeug.security import generate_password_hash, check_password_hash
+kimport sqlite3
+
 
 DB = "zgrom.db"
 
@@ -8,13 +8,51 @@ def connect():
     return sqlite3.connect(DB)
 
 
-def init_db(c.execute("""
+def init_db():
+
+    db = connect()
+    c = db.cursor()
+
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS users(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT UNIQUE,
+        password TEXT
+    )
+    """)
+
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS posts(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user TEXT,
+        text TEXT,
+        img TEXT,
+        likes INTEGER DEFAULT 0,
+        dislikes INTEGER DEFAULT 0
+    )
+    """)
+
+
+    c.execute("""
+    CREATE TABLE IF NOT EXISTS messages(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        sender TEXT,
+        receiver TEXT,
+        text TEXT
+    )
+    """)
+
+
+    c.execute("""
     CREATE TABLE IF NOT EXISTS groups(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        name TEXT UNIQUE,
+        name TEXT,
         owner TEXT
     )
     """)
+
 
     c.execute("""
     CREATE TABLE IF NOT EXISTS group_members(
@@ -24,6 +62,7 @@ def init_db(c.execute("""
     )
     """)
 
+
     c.execute("""
     CREATE TABLE IF NOT EXISTS group_posts(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -32,77 +71,8 @@ def init_db(c.execute("""
         text TEXT,
         media TEXT
     )
-    """) ):
-    db = connect()
-    c = db.cursor()
-
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS users(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT UNIQUE,
-        password TEXT,
-        admin INTEGER DEFAULT 0,
-        verified INTEGER DEFAULT 0
-    )
     """)
 
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS posts(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        username TEXT,
-        text TEXT,
-        image TEXT,
-        likes INTEGER DEFAULT 0
-    )
-    """)
-
-    c.execute("""
-    CREATE TABLE IF NOT EXISTS messages(
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        sender TEXT,
-        receiver TEXT,
-        message TEXT
-    )
-    """)
 
     db.commit()
     db.close()
-
-
-def create_user(username,password):
-
-    db=connect()
-    c=db.cursor()
-
-    try:
-        c.execute(
-        "INSERT INTO users(username,password) VALUES(?,?)",
-        (
-        username,
-        generate_password_hash(password)
-        ))
-
-        db.commit()
-        return True
-
-    except:
-        return False
-
-
-def login_user(username,password):
-
-    db=connect()
-    c=db.cursor()
-
-    c.execute(
-    "SELECT password FROM users WHERE username=?",
-    (username,)
-    )
-
-    user=c.fetchone()
-    db.close()
-
-    if user:
-        return check_password_hash(user[0],password)
-
-    return False
