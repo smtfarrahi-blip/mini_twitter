38e0b21cf6e09a16f45580a4a5de0aac9745d186
smtfarrahi-blip <s.mt.farrahi@gmail.com>
2026-06-21 14:09:14 +0330

    fix Zweez routes

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
-
-
-
-from flask import Flask, render_template
-from posts import get_posts
+from flask import Flask, render_template, request, redirect
+from posts import posts
+from auth import auth
+from groups import groups
 
 app = Flask(__name__)
 
+app.secret_key = "zweez_secret"
+
+app.register_blueprint(auth)
+app.register_blueprint(posts)
+app.register_blueprint(groups)
+
+
 @app.route("/")
 def home():
-    posts = get_posts()
-    return render_template("home.html", posts=posts)
+    return render_template("index.html")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
--- posts.py
+++ posts.py
@@ -1,11 +1,45 @@
+from flask import Blueprint, request, redirect, render_template
 import sqlite3
 
-def get_posts():
+posts = Blueprint("posts", __name__)
+
+
+@posts.route("/post", methods=["POST"])
+def create_post():
+
+    text = request.form.get("text")
+    username = "user"
+
     conn = sqlite3.connect("zgrom.db")
     c = conn.cursor()
 
-    c.execute("SELECT username, text, image, likes FROM posts ORDER BY id DESC")
-    posts = c.fetchall()
+    c.execute(
+        "INSERT INTO posts(username,text,likes) VALUES(?,?,?)",
+        (username,text,0)
+    )
+
+    conn.commit()
+    conn.close()
+
+    return redirect("/")
+
+
+@posts.route("/posts")
+def show_posts():
+
+    conn = sqlite3.connect("zgrom.db")
+    c = conn.cursor()
+
+    c.execute(
+        "SELECT username,text,image,likes FROM posts"
+    )
+
+    data = c.fetchall()
 
     conn.close()
-    return posts
+
+    return render_template(
+        "index.html",
+        posts=data
+    )
+
